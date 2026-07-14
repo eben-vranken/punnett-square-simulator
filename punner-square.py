353eee2e82631025734a1bcc8d309a79ab93cb77
eben-vranken <vranken.eben@outlook.com>
2026-07-14 23:32:10 +0200

    feat: initial parent genotype input

--- punner-square.py
+++ punner-square.py
@@ -1,5 +1,15 @@
+from argparse import ArgumentParser
+
 def parse_args():
-    pass
+    parser = ArgumentParser()
+
+    parser.add_argument("p1", help="Parental genotypes of parent 1")
+    parser.add_argument("p2", help="Parental genotypes of parent 2")
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
+
+    print(args.p1)
+    print(args.p2)
