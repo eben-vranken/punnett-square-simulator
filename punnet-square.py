0bd0f7354b5eb899e8e71b1373f81fe0256200f7
eben-vranken <vranken.eben@outlook.com>
2026-07-16 11:16:42 +0200

    feat: catch numerical input

--- punnet-square.py
+++ punnet-square.py
@@ -11,6 +11,10 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
+    if any(char.isdigit() for char in args.p1) or any(char.isdigit() for char in args.p2):
+        print("Parent genotypes cannot contain numerical characters.")
+        exit(1)
+
     if args.p1.upper() != args.p2.upper():
         print("Parent genotypes are not of the same type.\nInput a monohybrid cross.")
         exit(1)
