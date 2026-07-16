44053c72cdb3f58c364ea81f7b4464cae9b476aa
eben-vranken <vranken.eben@outlook.com>
2026-07-16 11:30:09 +0200

    feat: split up parent allele

--- src/simulator.py
+++ src/simulator.py
@@ -1,2 +1,7 @@
 def calculate(p1, p2):
-    print(p1, p2)
+    allele_p1 = p1[:len(p1)//2 + len(p1)%2], p1[len(p1)//2 + len(p1)%2:]
+
+    allele_p2 = p2[:len(p2)//2 + len(p2)%2], p2[len(p2)//2 + len(p2)%2:]
+
+    print(allele_p1)
+    print(allele_p2)
