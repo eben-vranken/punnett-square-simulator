22d2a882299a7bd5de4c5f39a0bde23ae1b3f031
eben-vranken <vranken.eben@outlook.com>
2026-07-16 12:01:47 +0200

    feat: initial punnett_square calculator for monohybrid crosses

--- src/simulator.py
+++ src/simulator.py
@@ -1,7 +1,20 @@
 def calculate(p1, p2):
-    allele_p1 = p1[:len(p1)//2 + len(p1)%2], p1[len(p1)//2 + len(p1)%2:]
+    allele_p1 = [char for char in p1]
+    allele_p2 = [char for char in p2]
 
-    allele_p2 = p2[:len(p2)//2 + len(p2)%2], p2[len(p2)//2 + len(p2)%2:]
+    punnett_square = []
 
-    print(allele_p1)
-    print(allele_p2)
+    for i in range(0, len(allele_p1)):
+        row = []
+        for j in range(0, len(allele_p2)):
+            row.append(allele_p1[i] + allele_p2[j])
+        punnett_square.append(row)
+
+    # Sort punnett square
+    for i in range(0, len(punnett_square)):
+        for j in range(0, len(punnett_square[i])):
+            sorted_chars = sorted(punnett_square[i][j],key=str.islower)
+
+            punnett_square[i][j] = "".join(sorted_chars)
+
+    print(punnett_square)
